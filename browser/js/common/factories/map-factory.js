91101feb75506d3223aa5d502e21f534a0d1e38c
Andrew Ash <andrewash9@gmail.com>
2016-03-17 15:52:07 -0400

    testing stuff

---
+++ browser/js/common/factories/map-factory.js
@@ -0,0 +1,82 @@
+app.factory('MapFactory', function () {
+  function Board(option) {
+    this.nodes = {};
+    this.current = null;
+    this.end = null;
+
+    if (typeof option === 'object') {
+      var self = this;
+      // creates all nodes
+      option.forEach(function (element, index) {
+          self.nodes[index] = new Node(index);
+        })
+        // assigns the connections to those created nodes
+      option.forEach(function (element, index) {
+        self.connect(index, element)
+      })
+    }
+    if (typeof option === 'number') {
+      for (let i = 0; i < option; i++) {
+        this.nodes[i] = new Node(i);
+      }
+    }
+
+    console.log('MADE A BOARD');
+  }
+
+  Board.prototype.addNode = function (nodeId) {
+    this.nodes[nodeId] = new Node(nodeId);
+    console.log('ADDED A NODE', this.nodes[nodeId])
+  }
+
+  Board.prototype.setCurrentAndEnd = function (currentId, endId) {
+    this.current = this.nodes[currentId];
+    if (endId) this.end = this.nodes[endId];
+    console.log('SET CURRENT AND END', this.current, this.end)
+  }
+
+  Board.prototype.setCurrent = function (currentId) {
+    this.current = this.nodes[currentId];
+  }
+
+  function Node(id) {
+    this.id = id;
+    this.red = null;
+    this.blue = null;
+    this.green = null;
+    this.gems = 0;
+    this.orange = false;
+    this.purple = false;
+  }
+
+  Board.prototype.connect = function (nodeId, objectOfConnections) { //(3, {red: 1, green: 5, blue: 6})
+    var nodeToConnect = this.nodes[nodeId];
+    for (var key in objectOfConnections) {
+      nodeToConnect[key] = objectOfConnections[key];
+    }
+    console.log(nodeToConnect + " IS CONNECTED TO " + nodeToConnect.red, nodeToConnect.green, nodeToConnect.blue);
+  }
+
+  Node.prototype.setGems = function (number) {
+    this.gems = number;
+  }
+
+  Node.prototype.setTroll = function (color) {
+    this[color] = true;
+  }
+
+  var MapFactory = {
+    createNewBoard: function () {
+      return new Board();
+    },
+    createNewNode: function (nodeId) {
+      return new Node(nodeId);
+    }
+  };
+
+  return MapFactory;
+});
+
+for (var i = 0; i < 10; i++) {
+  console.log(i)
+}
--- tests/browser/fsa-prebuilt/map-factory-test.js
+++ tests/browser/fsa-prebuilt/map-factory-test.js
@@ -7,47 +7,60 @@ describe('MapFactory', function () {
 
   beforeEach('inject tools', inject(function (_MapFactory_) {
     MapFactory = _MapFactory_;
-    level1 = new MapFactory.Board(6);
+    level1 = new MapFactory.createNewBoard(6);
   }));
 
   describe('Board', function () {
-    describe('constructor', function () {
+    describe('constructor method', function () {
       it('should be an object', function () {
         expect(level1).to.be.an('object');
       });
 
       it('should have as many nodes as the constructor call specifies', function () {
-        expect(level1.nodes.length).to.be(6);
+        console.dir("LEVEL 1 NODES: ", level1.nodes);
+        expect([].slice.call(level1.nodes).length).to.be(6);
       });
     });
 
     describe('connect method', function () {
       it('connects nodes properly', function () {
-        level1.connect(0, 1, 'red');
-        level1.connect(0, 2, 'green');
-        level1.connect(0, 4, 'blue');
-        level1.connect(1, 0, 'red');
-        level1.connect(1, 3, 'green');
-        level1.connect(1, 2, 'blue');
-        level1.connect(2, 5, 'red');
-        level1.connect(2, 0, 'green');
-        level1.connect(2, 1, 'blue');
-        level1.connect(3, 4, 'red');
-        level1.connect(3, 1, 'green');
-        level1.connect(3, 5, 'blue');
-        level1.connect(4, 3, 'red');
-        level1.connect(4, 5, 'green');
-        level1.connect(4, 0, 'blue');
-        level1.connect(5, 2, 'red');
-        level1.connect(5, 4, 'green');
-        level1.connect(5, 3, 'blue');
+        level1.connect(0, {
+          red: 1,
+          green: 2,
+          blue: 4
+        });
+        level1.connect(1, {
+          red: 0,
+          green: 3,
+          blue: 2
+        });
+        level1.connect(2, {
+          red: 5,
+          green: 0,
+          blue: 1
+        });
+        level1.connect(3, {
+          red: 4,
+          green: 1,
+          blue: 5
+        });
+        level1.connect(4, {
+          red: 3,
+          green: 5,
+          blue: 0
+        });
+        level1.connect(5, {
+          red: 2,
+          green: 4,
+          blue: 3
+        });
         expect(level1.nodes[0].red).to.be(1);
         expect(level1.nodes[2].blue).to.be(1);
         expect(level1.nodes[4].red).to.be(3);
       });
     });
 
-    describe('step method')
+    // describe('step method')
   });
 
 });
