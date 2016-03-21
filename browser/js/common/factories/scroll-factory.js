cb4e3536556a7cd484dc8ef0139be778034880ec
Vincent Leombruno <vleombruno42@gmail.com>
2016-03-21 10:59:45 -0400

    test

--- browser/js/common/factories/scroll-factory.js
+++ browser/js/common/factories/scroll-factory.js
@@ -1,28 +1,79 @@
 app.factory('ScrollFactory', function () {
-    function Scroll () {
-        this.instructions = {};
-        this.head;
+    function Scroll (option) {
+        this.items = {};
+        this.start = new Start();
+        this.end = new End();
+        this.pointer = this.start;
     }
 
-    Scroll.prototype.addInstruction = function (id, color) {
-        this.instructions[id] = new Instruction(id, color);
+    Scroll.prototype.addInstruction = function (id) {
+        this.items[id] = new Instruction(id);
     }
 
-    Scroll.prototype.addConditional = function (id, condition) {
-        this.instructions[id].condition = condition;
+    Scroll.prototype.addConditional = function (id) {
+        this.items[id] = new Conditional(id);
+    }
+
+    Scroll.prototype.setRoute = function (sourceId, str, destination) { // (3, "falsePath", 7)
+        this.items[sourceId][str] = destination;
     }
 
     Scroll.prototype.setColor = function (id, color) {
-        this.instructions[id].color = color;
+        if ("color" in this.items[id]) this.items[id].color = color;
+        else throw new Error("Cannot set color.")
     }
 
-    function Instruction (id, color) {
-        this.id = id;
-        this.color = color || null;
-        this.condition = null;
-        this.path1 = null;
-        this.path2 = null;
+    Scroll.prototype.setCondition = function (id, condition) {
+        if("condition" in this.items[id]) this.items[id].condition = condition;
+        else throw new Error("Cannot set condition.")
     }
+
+    Scroll.prototype.peek = function () {
+        if (this.pointer.next.constructor === Instruction) {
+            return this.pointer.next;
+        }
+        else if (this.pointer.next.id === "end") {
+
+        }
+    }
+
+    Scroll.prototype.move = function () {
+        //if this this.pointer.next === 
+
+
+
+        this.pointer = this.pointer.next;
+    }
+
+    function Instruction (id) {
+        this.id = id;
+        this.color = null;
+        this.next = null;
+    }
+
+    function Conditional (id) {
+        this.id = id;
+        this.truePath = null;
+        this.falsePath = null;
+        this.condition = null;
+    }
+
+    function Start () {
+        this.id = "start";
+        this.next = null;
+    }
+
+    function End () {
+        this.id = "end";
+    }
+
+
+
+
+
+
+
+
 
     var ScrollFactory = {
         createScroll: function () {
