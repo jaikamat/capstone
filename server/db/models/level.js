95b2ee619bd4c523a2903c7eb891a5032787a1d2
Vincent Leombruno <vleombruno42@gmail.com>
2016-03-22 16:06:38 -0400

    starting db models

---
+++ server/db/models/level.js
@@ -0,0 +1,10 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+	map: {type: Schema.Types.ObjectId, ref: 'Map'},
+	scroll: {type: Schema.Types.ObjectId, ref: 'Scroll'},
+	params: {type: Schema.Types.ObjectId, ref: 'Params'}
+})
+
+mongoose.model('Level', schema);
