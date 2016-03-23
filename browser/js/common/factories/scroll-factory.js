c8c4c4354dd42766ba36012a3d3aa6f1eb7560e3
Andrew Ash <andrewash9@gmail.com>
2016-03-23 14:45:07 -0400

    linked up to EvalFactory

--- browser/js/common/factories/scroll-factory.js
+++ browser/js/common/factories/scroll-factory.js
@@ -108,7 +108,8 @@ app.factory('ScrollFactory', function () {
     }
 
     Scroll.prototype.getData = function (gameData) {
-        if (this.pointer.id === "start") this.pointer = this.items[this.pointer.next];    //If the pointer is at start, move the pointer.
+        console.log("Pointer: ", this.pointer);
+        if (this.pointer.id === "start") this.pointer = this.start.next;    //If the pointer is at start, move the pointer.
         if (this.pointer.constructor === Conditional) this.move(gameData);                //If you are then currently on a conditional, execute move
         if (this.pointer.constructor === Instruction) {                                   //If you are then currently on an Instruction,
             var data = this.pointer.color;                                                //Store the pointer data
@@ -124,7 +125,10 @@ app.factory('ScrollFactory', function () {
     };
 
     Scroll.prototype.move = function (gameData) {                                         //{trollStatus: 'orange', gemsCollected: 2}
-        if (!gameData) this.pointer = this.items[this.pointer.next];
+        if (!gameData) {
+            if (this.pointer.next.id === -1) this.pointer = this.end;
+            else this.pointer = this.items[this.pointer.next];
+        }
         else {
             if (gameData.trollStatus === this.pointer.condition || gameData.gemsCollected === this.pointer.condtion) {
                 this.pointer = this.items[this.pointer.truePath];                         //move to True Path
--- server/db/models/level.js
+++ server/db/models/level.js
@@ -1,5 +1,6 @@
 'use strict';
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var schema = new mongoose.Schema({
   	level: {type: Number, min: 1, required: true, unique: true},
