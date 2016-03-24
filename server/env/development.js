68ea1211de265e4398ddfe9a8e6c53d34082afaf
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-03-24 14:01:43 -0400

    added google user login with official google sign in button

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+  "DATABASE_URI": "mongodb://localhost:27017/capstone",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
@@ -12,8 +12,8 @@ module.exports = {
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    "clientID": "536931443069-c96t705hv8vl1u8nnepf6664mo6ne86u.apps.googleusercontent.com",
+    "clientSecret": "_YxeTC6OSmq1_gMYwU5YdI3k",
+    "callbackURL": "http://127.0.0.1:1337/auth/google/callback"
   }
 };
