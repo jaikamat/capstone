1bbe679e9622b56e3e5b3067847967613641dccf
Vincent Leombruno <vleombruno42@gmail.com>
2016-03-24 12:12:25 -0400

    added difficulty to level model

--- server/db/models/level.js
+++ server/db/models/level.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 
 var schema = new mongoose.Schema({
   	level: {type: Number, min: 1, required: true, unique: true},
+  	difficulty: {type: String, enum: ['Beginner', 'Intermediate', 'Advanced', 'Expert']},
 	map: {type: Schema.Types.ObjectId, ref: 'Map', required: true},
 	scroll: {type: Schema.Types.ObjectId, ref: 'Scroll', required: true},
 	params: {type: Schema.Types.ObjectId, ref: 'Params', required: true}
