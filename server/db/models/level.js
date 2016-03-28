4ba584500782f07d8e2c6d6a79a83bd37659437d
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-03-27 23:46:29 -0400

    made user-stats model

--- server/db/models/level.js
+++ server/db/models/level.js
@@ -5,9 +5,9 @@ var Schema = mongoose.Schema;
 var schema = new mongoose.Schema({
   	levelId: {type: Number, min: 1, required: true, unique: true},
   	difficulty: {type: String, enum: ['Beginner', 'Intermediate', 'Advanced', 'Expert']},
-	map: {type: Schema.Types.ObjectId, ref: 'Maps', required: true},
-	scroll: {type: Schema.Types.ObjectId, ref: 'Scrolls', required: true},
-	params: {type: Schema.Types.ObjectId, ref: 'Params', required: true}
+	  map: {type: Schema.Types.ObjectId, ref: 'Maps', required: true},
+	  scroll: {type: Schema.Types.ObjectId, ref: 'Scrolls', required: true},
+	  params: {type: Schema.Types.ObjectId, ref: 'Params', required: true}
 });
 
 mongoose.model('Levels', schema);
